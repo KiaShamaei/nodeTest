8f2e710e0fb09792f6170ecc5a805d2f70c273fe
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-06-28 08:12:40 +0430

    read & write

--- node/file.js
+++ node/file.js
@@ -1,8 +1,41 @@
 const fs = require('fs')
 
+//read file -----------------------------------------
+// fs.readFile('./document/doc.txt', (err,data)=>{
+//     if(err)
+//     console.log("file err", err)
+//     console.log(data.toString())
+// })
+//read file end --------------------------------------
+//write file -----------------------------------------
+// fs.writeFile('./document/doc2.txt', "this is from write file", (value)=>{
+// 	console.log(value, "new file write!")
+// })
+//write file end--------------------------------------
 
-fs.readFile('./document/doc.txt', (err,data)=>{
-    if(err)
-    console.log("file err", err)
-    console.log(data.toString())
+//dirctories -----------------------------------------
+
+if (!fs.existsSync('./assets')){
+	fs.mkdir("./assets", (err)=>{
+		if(err){
+			console.log(err)
+		}
+		console.log("new folder created")
+	})
+}else {
+	fs.rmdir('./assets', (err)=>{
+		if(err)console.log(err)
+		console.log("remove file done!")
+	})
+}
+
+
+//deleting file ----------------------------
+fs.unlink('./document/deleteme.txt' , (err)=>{
+	if(err) fs.writeFile('document/deleteme.txt', "" , ()=>{
+		
+	})
+	console.log("the file is deleted")
 })
+
+
